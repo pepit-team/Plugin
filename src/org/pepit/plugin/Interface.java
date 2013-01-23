05baf8f227ec1724c4a8e1314461ead2c5f7fe10
Stephane Legrand <stephane@stephane-System-Product-Name.(none)>
2013-01-23 21:06:15 +0100

    Remove the "File rootResource" parameter. It's now useless as we have the zip utils methods.

--- src/org/pepit/plugin/Interface.java
+++ src/org/pepit/plugin/Interface.java
@@ -23,131 +23,122 @@
 
 package org.pepit.plugin;
 
-import java.io.File;
-
 import android.content.Context;
 import android.widget.LinearLayout;
 
 public interface Interface {
 
-    /**
-     * Return plugin metadata.
-     * 
-     * @return Metadata
-     */
-    public Info getInfo();
+	/**
+	 * Return plugin metadata.
+	 * 
+	 * @return Metadata
+	 */
+	public Info getInfo();
 
-    /**
-     * Return a layout to display a presentation of the plugin (an image and one
-     * line of text for instance).
-     * 
-     * @param ctx
-     *            Current Context
-     * @param rootResource
-     *            Root directory for resources
-     * @return Layout to display
-     */
-    public LinearLayout getExercisePresentationLayout(Context ctx,
-	    File rootResource);
+	/**
+	 * Return a layout to display a presentation of the plugin (an image and one
+	 * line of text for instance).
+	 * 
+	 * @param ctx
+	 *            Current Context
+	 * @return Layout to display
+	 */
+	public LinearLayout getExercisePresentationLayout(Context ctx);
 
-    /**
-     * Return the exercises for the plugin.
-     * 
-     * @return Exercises list
-     */
-    public String[] getExerciseList();
+	/**
+	 * Return the exercises for the plugin.
+	 * 
+	 * @return Exercises list
+	 */
+	public String[] getExerciseList();
 
-    /**
-     * Return a layout to display an explanation of the plugin (how to play in
-     * one screen).
-     * 
-     * @param ctx
-     *            Current Context
-     * @param rootResource
-     *            Root directory for resources
-     * @param selectedExercise
-     *            Number of selected exercise
-     * @return Layout to display
-     */
-    public LinearLayout getExplanationPresentationLayout(Context ctx,
-	    File rootResource, int selectedExercise);
+	/**
+	 * Return a layout to display an explanation of the plugin (how to play in
+	 * one screen).
+	 * 
+	 * @param ctx
+	 *            Current Context
+	 * @param selectedExercise
+	 *            Number of selected exercise
+	 * @return Layout to display
+	 */
+	public LinearLayout getExplanationPresentationLayout(Context ctx,
+			int selectedExercise);
 
-    /**
-     * Return the modules for an exercise.
-     * 
-     * @param exercise
-     *            Number of exercise
-     * @return Modules list
-     */
-    public String[] getModuleList(int exercise);
+	/**
+	 * Return the modules for an exercise.
+	 * 
+	 * @param exercise
+	 *            Number of exercise
+	 * @return Modules list
+	 */
+	public String[] getModuleList(int exercise);
 
-    /**
-     * Return a layout to display one question.
-     * 
-     * @param ctx
-     *            Current Context
-     * @param rootResource
-     *            Root directory for resources
-     * @param selectedExercise
-     *            Number of selected exercise
-     * @param selectedModule
-     *            Number of selected module
-     * @param numQuestion
-     *            Number of question
-     * @return Layout to display
-     */
-    public LinearLayout getQuestionLayout(Context ctx, File rootResource,
-	    int selectedExercise, int selectedModule, int numQuestion);
+	/**
+	 * Return a layout to display one question.
+	 * 
+	 * @param ctx
+	 *            Current Context
+	 * @param selectedExercise
+	 *            Number of selected exercise
+	 * @param selectedModule
+	 *            Number of selected module
+	 * @param numQuestion
+	 *            Number of question
+	 * @return Layout to display
+	 */
+	public LinearLayout getQuestionLayout(Context ctx, int selectedExercise,
+			int selectedModule, int numQuestion);
 
-    /**
-     * Return the number of question(s) for an exercise and a module.
-     * 
-     * @param selectedExercise
-     *            Number of selected exercise
-     * @param selectedModule
-     *            Number of selected module
-     * @return Number of question(s)
-     */
-    public int getQuestionCount(int selectedExercise, int selectedModule);
+	/**
+	 * Return the number of question(s) for an exercise and a module.
+	 * 
+	 * @param selectedExercise
+	 *            Number of selected exercise
+	 * @param selectedModule
+	 *            Number of selected module
+	 * @return Number of question(s)
+	 */
+	public int getQuestionCount(int selectedExercise, int selectedModule);
 
-    /**
-     * Method called before starting a sequence of questions.
-     */
-    public void startQuestionSequence();
+	/**
+	 * Method called before starting a sequence of questions.
+	 */
+	public void startQuestionSequence();
 
-    /**
-     * Method called after the end of a sequence of questions.
-     */
-    public void finishQuestionSequence();
+	/**
+	 * Method called after the end of a sequence of questions.
+	 */
+	public void finishQuestionSequence();
 
-    /**
-     * Text to display in the "Next question" button.
-     * 
-     * @return Text
-     */
-    public String getNextQuestionButtonText();
+	/**
+	 * Text to display in the "Next question" button.
+	 * 
+	 * @return Text
+	 */
+	public String getNextQuestionButtonText();
 
-    /**
-     * Check if the current answer is the right one.
-     * 
-     * @return TRUE if right
-     */
-    public boolean currentAnswerIsRight();
+	/**
+	 * Check if the current answer is the right one.
+	 * 
+	 * @return TRUE if right
+	 */
+	public boolean currentAnswerIsRight();
 
-    /**
-     * Ask the plugin to show to the user that her current answer is right.
-     */
-    public void showAnswerIsRight();
+	/**
+	 * Ask the plugin to show to the user that her current answer is right.
+	 */
+	public void showAnswerIsRight();
 
-    /**
-     * Ask the plugin to show to the user that her current answer is wrong.
-     */
-    public void showAnswerIsWrong();
+	/**
+	 * Ask the plugin to show to the user that her current answer is wrong.
+	 */
+	public void showAnswerIsWrong();
 
-    /**
-     * Return the current global score for the plugin.
-     * 
-     * @return Score
-     */
-    public int getScore();
+	/**
+	 * Return the current global score for the plugin.
+	 * 
+	 * @return Score
+	 */
+	public int getScore();
 }
